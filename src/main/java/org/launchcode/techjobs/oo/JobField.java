aa0fc77b50857207e3e5d9594b3795a73f293e28
luciemariec <132121627+luciemariec@users.noreply.github.com>
2024-05-16 20:14:59 -0500

    Finished Task 6

---
+++ src/main/java/org/launchcode/techjobs/oo/JobField.java
@@ -0,0 +1,55 @@
+package org.launchcode.techjobs.oo;
+
+import java.util.Objects;
+
+public abstract class JobField {
+    private int id;
+    private static int nextId = 1;
+    public String value;
+
+    //Code Constructors
+    public JobField() {
+        id = nextId;
+        nextId++;
+    }
+
+    public JobField(String value) {
+        this();
+        this.value = value;
+    }
+
+    // Getters and setters
+    public int getId() {
+        return id;
+    }
+
+    public static int getNextId() {
+        return nextId;
+    }
+
+    public String getValue() {
+        return value;
+    }
+
+    //Equals and hashCode methods
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        JobField jobField = (JobField) o;
+        return id == jobField.id && Objects.equals(value, jobField.value);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, value);
+    }
+
+    //Commonly used toString method
+    @Override
+    public String toString() {
+        return value;
+    }
+}
+
+
